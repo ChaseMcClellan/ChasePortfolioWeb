e5f1d1d3837976ff14509dedf14183a79fabe12b
Chase McClellan <mcclel54@uwm.edu>
2024-08-08 23:10:21 -0500

    personal projects are done for now

--- ChasePortfolioWeb/Services/ProjectService.cs
+++ ChasePortfolioWeb/Services/ProjectService.cs
@@ -10,12 +10,26 @@ namespace ChasePortfolioWeb.Services
         {
             new Project
             {
-                Title = "Project 1",
-                Description = "Description for Project 1",
-                ImageUrl = "/images/project1.jpg",
-                ProjectUrl = "https://github.com/yourusername/project1"
+                Title = "Random Sound Discord Bot",
+                Description = "Produced a simple soundboard bot using Discord’s development tools. Used C# programming and Discord API. " +
+                "Includes simple command triggers and basic discord bot functions.",
+                ImageUrl = "https://static-00.iconduck.com/assets.00/discord-icon-2048x2048-o5mluhz2.png",
+                ProjectUrl = "https://github.com/ChaseMcClellan/DiscordSoundBot"
             },
-            // Add more projects as needed
+            new Project
+            {
+                Title = "ASP.Net Full stack website",
+                Description = "Engineered a full stack application using ASP.Net framework. Designed a relational database hosted on Azure. Implemented features such as login, administrative roles, and shopping cart.\r\n",
+                ImageUrl = "https://devblogs.microsoft.com/aspnet/wp-content/uploads/sites/16/2019/04/BrandBlazor_nohalo_1000x.png",
+                ProjectUrl = "https://github.com/ChaseMcClellan/ChaseWebsite.git"
+            },
+            new Project
+            {
+                Title = "ASP.Net CORE Personal Portfolio",
+                Description = "Engineered a portfolio application using ASP.Net framework. Using MVC logic, a fully interactive website for the user \r\n",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/1024px-.NET_Core_Logo.svg.png",
+                ProjectUrl = ""
+            }
         };
         }
     }
