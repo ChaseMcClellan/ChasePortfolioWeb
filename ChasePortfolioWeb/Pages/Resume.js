8f5e87e9293afa75711756000eff4f52a827ef59
Chase McClellan <mcclel54@uwm.edu>
2024-09-29 23:11:31 -0500

    Added js for resume page, resume pdf

---
+++ ChasePortfolioWeb/Pages/Resume.js
@@ -0,0 +1,8 @@
+﻿document.getElementById('download-resume').addEventListener('click', function () {
+    const link = document.createElement('a');
+    link.href = '/docs/Chase_McClellan_Resume.pdf';  // Ensure this path points to the correct location of the file in your project
+    link.download = 'Chase_McClellan_Resume.pdf';  // This is the name the file will be saved as
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
