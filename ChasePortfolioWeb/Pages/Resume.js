5324e1ab5874fbd6e4f5375944fb79d91cce28c0
Chase McClellan <mcclel54@uwm.edu>
2024-09-29 23:46:59 -0500

    algorithm works, fixed grammer

--- ChasePortfolioWeb/Pages/Resume.js
+++ ChasePortfolioWeb/Pages/Resume.js
@@ -1,6 +1,6 @@
 ﻿document.getElementById('download-resume').addEventListener('click', function () {
     const link = document.createElement('a');
-    link.href = '/docs/Chase_McClellan_Resume.pdf';  // Ensure this path points to the correct location of the file in your project
+    link.href = '~/docs/Chase_McClellan_Resume.pdf';  // Ensure this path points to the correct location of the file in your project
     link.download = 'Chase_McClellan_Resume.pdf';  // This is the name the file will be saved as
     document.body.appendChild(link);
     link.click();
